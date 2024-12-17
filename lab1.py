b354c13f4a39df9b8bddf9de128eadfcc4120942
turboss <143201594+turbososyn@users.noreply.github.com>
2024-12-17 15:54:12 +0200

    fixed, added last comments

--- lab1.py
+++ lab1.py
@@ -1,8 +1,8 @@
 import asyncio
 import time
 from typing import Callable, List, Any
-# A debounce function that adds a delay if the task is running very quickly
 
+#A debounce function that adds a delay if the task is running very quickly
 async def debounce(func: Callable, delay: float, *args, **kwargs):
     start_time = time.time()
     result = await func(*args, **kwargs)
@@ -16,14 +16,21 @@ async def debounce(func: Callable, delay: float, *args, **kwargs):
 async def async_map(func: Callable, iterable: List[Any]) -> List[Any]:
     tasks = [func(item) for item in iterable]
     return await asyncio.gather(*tasks)
+
 #An example of an asynchronous function
 async def async_square(x: int) -> int:
     await asyncio.sleep(2) 
     return x * x
+
+#Demonstration of work
 async def demo():
     numbers = [1, 2, 3, 4, 5]
+    #Call asynchronous map
     results = await async_map(async_square, numbers)
     print(f"Results after async_map: {results}")
+    #Call the debounce function
     debounced_results = await debounce(async_map, 3, async_square, numbers)
     print(f"Results after debounce: {debounced_results}")
+
+# Launch the demo
 asyncio.run(demo())
