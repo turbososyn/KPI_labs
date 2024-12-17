8b95106ce57ef9c3a20baf405fabf0e522c23097
turboss <143201594+turbososyn@users.noreply.github.com>
2024-12-17 14:56:32 +0200

    added function debounce

---
+++ lab1.py
@@ -0,0 +1,10 @@
+import asyncio
+import time
+from typing import Callable, List, Any
+async def debounce(func: Callable, delay: float, *args, **kwargs):
+    start_time = time.time()
+    result = await func(*args, **kwargs)
+    elapsed_time = time.time() - start_time
+    if elapsed_time < delay:
+        await asyncio.sleep(delay - elapsed_time)
+    return result
