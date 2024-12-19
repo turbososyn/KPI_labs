e7c05616fe98902f8b446920eab21752e9304011
turboss <143201594+turbososyn@users.noreply.github.com>
2024-12-19 14:42:39 +0200

    added example of an async operation that can be canceled

--- lab3.py
+++ lab3.py
@@ -9,5 +9,14 @@ async def async_map(arr, async_fn):
     try:
         await asyncio.gather(*tasks)
     except asyncio.CancelledError:
-        print("Операцію було скасовано.")
+        print("The operation was cancelled.")
     return results
+async def async_operation(data, results, has_error):
+    try:
+        await asyncio.sleep(1)
+        if has_error:
+            raise asyncio.CancelledError("The operation was cancelled.")
+
+        results.append(data * 2)  
+    except asyncio.CancelledError:
+        raise 
