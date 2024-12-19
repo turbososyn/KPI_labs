c31f9e1eb8bac05afd628b811d64cc4de9abd272
turboss <143201594+turbososyn@users.noreply.github.com>
2024-12-19 16:40:02 +0200

    added main asyncfunction to start processing and starting an async loop

--- lab4.py
+++ lab4.py
@@ -9,3 +9,11 @@ async def process_data(start, end, filename):
         with open(filename, 'a') as f:
             f.write(f"{data} -> {result}\n")
         print(f"Processed: {data} -> {result}")
+async def main():
+    filename = "processed_data.txt"
+    start = 1
+    end = 101
+    open(filename, 'w').close()
+    await process_data(start, end, filename)
+if __name__ == "__main__":
+    asyncio.run(main())
